68bfa2160ea1235feb397171a528ee78cd50b9af
cmcwatta <74875036+cmcwatta@users.noreply.github.com>
2024-03-31 20:20:23 -0400

    Lab11
    Lab 11

--- game/Assets/FactoryDesign/AbstractLocationFactory.cs
+++
@@ -1,4 +0,0 @@
-public abstract class AbstractLocationFactory
-{
-    public abstract ILocation CreateLocation();
-}
--- game/Assets/FactoryDesign/LocationFactory/FactoryGrass.cs
+++
@@ -1,7 +0,0 @@
-public class FactoryGrass : AbstractLocationFactory
-{
-    public override ILocation CreateLocation()
-    {
-        return new LocationGrass();
-    }
-}
